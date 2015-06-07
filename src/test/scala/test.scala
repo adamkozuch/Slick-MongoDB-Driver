12328d597f6d78f547f531237969743e5c84d8ad
adam.kozuch@gmail.com <adam.kozuch@gmail.com>
2015-06-07 15:20:25 +0200

    empty test

--- src/test/scala/test.scala
+++ src/test/scala/test.scala
@@ -9,13 +9,21 @@ import scala.concurrent.ExecutionContext
 /**
  * Created by adam on 02.06.15.
  */
-//@RunWith(classOf[Testkit])
-//class FirstTest extends DriverTest(FirstTest.tdb)
+@RunWith(classOf[Testkit])
+class FirstTest extends DriverTest(FirstTest.tdb)
 
 object FirstTest extends App{
 
-  val t = new NestingTest
-  t.testNestedOptions
+val tdb = new RelationalTestDB {
+  type Driver = this.type
+
+  def dropUserArtifacts(implicit session: this.type#profile)
+
+  def createDB() = ???
+
+  val driver: Driver = _
+  val confName: String = _
+}
 //  def tdb = new RelationalTestDB {
 //    type Driver = MongoDriver
 //    val driver: Driver = MongoDriver
